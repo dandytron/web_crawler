6aa13583336c08f02dffbb5497f1a947d0e21059
dandytron <104713695+dandytron@users.noreply.github.com>
2024-08-28 17:00:30 +0800

    Added a function for extracting URLs from HTML tags

--- crawl.js
+++ crawl.js
@@ -1,4 +1,6 @@
-export function normalizeURL(s) {
+import { JSDOM } from 'jsdom'
+
+function normalizeURL(s) {
     let urlObj = new URL(s)
     let fullpath = `${urlObj.host}${urlObj.pathname}`
     if (fullpath.slice(-1) === '/') {
@@ -6,3 +8,32 @@ export function normalizeURL(s) {
     }
     return fullpath
 }
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urlList = []
+    // creates a new "document object model" or DOM
+    const dom = new JSDOM(htmlBody)
+    // returns an array of <a> tag "anchor" elements
+    const nodeList = dom.window.document.querySelectorAll('a')
+    // convert to an array
+    const anchorArray = Array.from(nodeList)
+    // loop through the list and add the URLs to the return array.
+    anchorArray.forEach(anchorTag =>{
+        if (anchorTag.hasAttribute('href')) {
+            let url = anchorTag.getAttribute('href')
+
+            try {
+                url = new URL(url, baseURL).href
+                urlList.push(url)
+            } catch(err) {
+                console.log('${err.message}: ${href}')
+            }
+            
+        }
+
+    })
+    return urlList
+}
+
+// we don't need the 'export function' keywords above if we choose to export as below:
+export { normalizeURL, getURLsFromHTML }
